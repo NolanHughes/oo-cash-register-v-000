be9d211376dcd4372b548f4b0346f9aca3571c34
Nolan Hughes <nhughes987@gmail.com>
2017-11-08 18:24:34 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,20 +7,20 @@ class CashRegister
     @total = 0
     @discount = discount
   end
-  #
-  #
-  # def add_item(title, price, quantity = 1)
-  #   self.total += price * quantity
-  # end
-  #
-  # def apply_discount
-  #   if self.discount == nil
-  #     "There is no discount to apply."
-  #   else
-  #     @total = total - (total * (discount * 0.01))
-  #     "After the discount, the total comes to $#{total.to_i}."
-  #   end
-  # end
+  
+  
+  def add_item(title, price, quantity = 1)
+    self.total += price * quantity
+  end
+  
+  def apply_discount
+    if self.discount == nil
+      "There is no discount to apply."
+    else
+      @total = total - (total * (discount * 0.01))
+      "After the discount, the total comes to $#{total.to_i}."
+    end
+  end
 end
 
 # cash_register_with_discount = CashRegister.new(20)
