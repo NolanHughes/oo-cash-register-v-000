70e25e1600d69806dc4084516a63e57991c71799
Nolan Hughes <nhughes987@gmail.com>
2017-11-08 19:08:25 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,21 +2,21 @@ require 'pry'
 
 # class CashRegister
 #   attr_accessor :total, :discount, :last_transaction
-# 
+#
 #   @@array_of_all_items = []
-# 
+#
 #   def initialize(discount = nil)
 #     @total = 0
 #     @discount = discount
-# 
+#
 #   end
-# 
+#
 #   def add_item(title, price, quantity = 1)
 #     @@array_of_all_items << title * quantity
 #     @last_transaction = price
 #     self.total += price * quantity
 #   end
-# 
+#
 #   def apply_discount
 #     if self.discount == nil
 #       "There is no discount to apply."
@@ -25,26 +25,26 @@ require 'pry'
 #       "After the discount, the total comes to $#{total.to_i}."
 #     end
 #   end
-# 
+#
 #   #get only specific items in array
 #   #multiple number of items by their quantity
 #   def items
 #     @@array_of_all_items
 #   end
-# 
+#
 #   def void_last_transaction
 #     @total = @total - @last_transaction
 #   end
-# 
+#
 # end
 
-@@array_of_all_items = []
+array_of_all_items = []
 
 def repeat(text, n=2)
-  @@array_of_all_items << [text] * n * ' '
+  array_of_all_items << [text] * n * ' '
 end
 
-repeat("tomato")
+
 
 # cash_register = CashRegister.new
 # cash_register.add_item("tomato", 1.76)
@@ -52,4 +52,4 @@ repeat("tomato")
 # # new_register.add_item("eggs", 1.99)
 # # new_register.add_item("tomato", 1.76, 3)
 # #
-# binding.pry
+binding.pry
