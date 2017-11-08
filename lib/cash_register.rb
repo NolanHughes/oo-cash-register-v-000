7ebbc9761b721613379b28efcdda135650d2cd8e
Nolan Hughes <nhughes987@gmail.com>
2017-11-08 18:06:39 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -24,5 +24,5 @@ end
 # cash_register_with_discount = CashRegister.new
 # cash_register_with_discount.add_item("macbook air", 1000)
 # cash_register_with_discount.apply_discount
-# 
+#
 # binding.pry
