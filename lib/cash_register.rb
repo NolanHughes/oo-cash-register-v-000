c806c09641e6784c02ee48b08e2fac647998e630
Nolan Hughes <nhughes987@gmail.com>
2017-11-08 19:07:02 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,42 +1,42 @@
 require 'pry'
 
-class CashRegister
-  attr_accessor :total, :discount, :last_transaction
-
-  @@array_of_all_items = []
-
-  def initialize(discount = nil)
-    @total = 0
-    @discount = discount
-
-  end
-
-  def add_item(title, price, quantity = 1)
-    @@array_of_all_items << title * quantity
-    @last_transaction = price
-    self.total += price * quantity
-  end
-
-  def apply_discount
-    if self.discount == nil
-      "There is no discount to apply."
-    else
-      @total = total - (total * (discount * 0.01))
-      "After the discount, the total comes to $#{total.to_i}."
-    end
-  end
-
-  #get only specific items in array
-  #multiple number of items by their quantity
-  def items
-    @@array_of_all_items
-  end
-
-  def void_last_transaction
-    @total = @total - @last_transaction
-  end
-
-end
+# class CashRegister
+#   attr_accessor :total, :discount, :last_transaction
+# 
+#   @@array_of_all_items = []
+# 
+#   def initialize(discount = nil)
+#     @total = 0
+#     @discount = discount
+# 
+#   end
+# 
+#   def add_item(title, price, quantity = 1)
+#     @@array_of_all_items << title * quantity
+#     @last_transaction = price
+#     self.total += price * quantity
+#   end
+# 
+#   def apply_discount
+#     if self.discount == nil
+#       "There is no discount to apply."
+#     else
+#       @total = total - (total * (discount * 0.01))
+#       "After the discount, the total comes to $#{total.to_i}."
+#     end
+#   end
+# 
+#   #get only specific items in array
+#   #multiple number of items by their quantity
+#   def items
+#     @@array_of_all_items
+#   end
+# 
+#   def void_last_transaction
+#     @total = @total - @last_transaction
+#   end
+# 
+# end
 
 @@array_of_all_items = []
 
