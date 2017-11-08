59ec44a762777e7b0cd300980baa53d709aceb33
Nolan Hughes <nhughes987@gmail.com>
2017-11-08 18:21:18 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,15 +3,15 @@ require 'pry'
 class CashRegister
   attr_accessor :total, :discount
 
-  def initialize(discount = 1)
+  def initialize(discount = nil)
     @total = 0
   end
-  # 
-  # 
+  #
+  #
   # def add_item(title, price, quantity = 1)
   #   self.total += price * quantity
   # end
-  # 
+  #
   # def apply_discount
   #   if self.discount == nil
   #     "There is no discount to apply."
